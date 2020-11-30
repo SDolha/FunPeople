6fc9bf8a39a4c941debf89bd9f214b03eee8c977
Sorin Dolha <SDolha@Hotmail.com>
2020-11-30 17:05:13 +0200

    Initial Commit

---
+++ FunPeople/FunPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunPeopleApp.swift
+//  FunPeople
+//
+//  Created by Sorin Dolha on 30.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct FunPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
