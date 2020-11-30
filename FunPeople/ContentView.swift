69baf6c0c7db7cdde72876aa0982fac6529f469d
Sorin Dolha <SDolha@Hotmail.com>
2020-11-30 17:10:40 +0200

    +

--- FunPeople/ContentView.swift
+++ FunPeople/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let actions = ["falls", "eats", "drinks", "climbes", "dreams"]
+    let complements = ["into the bushes", "shoes", "from a bag", "whenever possible"]
+    @State var name = ""
+    @State var statement = "Hello, World!"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Enter a friend's name", text: $name)
+                .frame(width: 200)
+                .multilineTextAlignment(.center)
+            Button(action: prepareStatement) {
+                Text("Tell me something about him/her")
+            }
+            Text(statement)
+        }
+    }
+    func prepareStatement() {
+        let action = actions[Int.random(in: 0..<actions.count)]
+        let complement = complements[Int.random(in: 0..<complements.count)]
+        statement = "\(name) \(action) \(complement)."
     }
 }
 
